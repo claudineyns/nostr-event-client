76f9b89f05644234cc4065267a69bdb347993550
Claudiney Nascimento <contato@claudiney.dev>
2023-09-05 22:39:13 -0300

    utilty

---
+++ nostr-event-check.js
@@ -0,0 +1,47 @@
+const { createHash } = require('node:crypto');
+const process = require('node:process');
+
+const Buffer     = require('safe-buffer').Buffer;
+const BigInteger = require('bigi');
+const schnorr    = require('bip-schnorr');
+
+const data = validate_event(JSON.parse(process.argv[2]));
+console.log(JSON.stringify(data));
+
+
+function validate_event(event) {
+
+  const id = event.id;
+  const event_id = create_event_id(event.pubkey, event.created_at, event.kind, event.tags, event.content);
+  if(id !== event_id) return {'status':false, 'message': 'id not match'};
+
+  const valid = check_sig(event.pubkey, event.id, event.sig);
+  if(!valid) return {'status':false, 'message': 'signature not match'};
+
+  return {'status':true, 'message':'OK'};
+}
+
+function check_sig(pubkey, event_id, sig) {
+
+  const public_key = Buffer.from(pubkey, 'hex')
+  const message    = Buffer.from(event_id, 'hex');
+  const signature  = Buffer.from(sig, 'hex');
+
+  try {
+    schnorr.verify(public_key, message, signature);
+    return true;
+  } catch (e) {
+    return false;
+  }
+
+}
+
+function create_event_id(pubkey, created_at, kind, tags, content) {
+    const hash = createHash('sha256');
+
+    const event_data = JSON.stringify([0, pubkey, created_at, kind, tags, content]);
+    hash.update(Buffer.from(event_data, 'utf8'));
+
+    const event_id = hash.digest('hex');
+    return event_id;
+}
